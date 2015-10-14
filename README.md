de585a1e101a12887298c1484305ffef05b4523f
l-vincent-l <lara.vincent@gmail.com>
2015-10-14 16:11:08 +0200

    Add form for apikey

--- README.md
+++ README.md
@@ -10,8 +10,7 @@ You need a python 2.7 and pip, and it's better if you have virtualenv on your ma
 
 Settings
 --------
-
-To generate the settings file you can run:
+If you want to have a default APIKEY you can generate the settings file by running:
 
 ```
 python make_settings.py
@@ -29,13 +28,17 @@ You can install dependencies by running this command
 
 ```
  pip install -r requirements.txt
-
  ```
 
 Running
 -------
 
 You can now run this command:
+```
+python run.py
+```
+
+Or if you have a config file:
 
 ```
 BASIC_SETTINGS=my_settings.py python run.py
--- run.py
+++ run.py
@@ -1,26 +1,39 @@
 #coding: utf-8
 from flask import Flask, render_template, request, redirect
 import requests, json, os, sys
+from copy import deepcopy
 
 app = Flask(__name__)
 app.config.from_object('default_settings')
 if 'BASIC_SETTINGS' in os.environ.keys():
     app.config.from_envvar('BASIC_SETTINGS')
 
-if app.config['X_API_KEY'] is None:
-    print "You need to provide an api key in settings\n"
-    sys.exit(1)
-
-
 
 headers_request = {
     "X-VERSION": 2,
     "Accept": "application/json",
-    "X-API-KEY": app.config['X_API_KEY']
 }
+
 
 headers_request_post = dict({"content-type": "application/json"}, **headers_request)
 base_url = app.config['BASE_URL']
+
+class SettingError(Exception):
+    pass
+
+def make_headers(is_post=False):
+    if is_post:
+        headers = deepcopy(headers_request_post)
+    else:
+        headers = deepcopy(headers_request)
+    headers['X-API-KEY'] = request.args.get('apikey', app.config['X_API_KEY'])
+    if headers['X-API-KEY'] is None:
+        raise SettingError
+    return headers
+
+@app.errorhandler(SettingError)
+def errorapikey(error):
+    return render_template('get_token.html', page=str(request.url_rule.rule))
 
 
 @app.route("/")
@@ -28,12 +41,14 @@ def index():
     get_location = 'lat' not in request.args.keys()
     lat = request.args.get('lat', 48.8)
     lon = request.args.get('lon', 2.3)
+    headers = make_headers()
     r = requests.get('{}/taxis/?lat={}&lon={}'.format(base_url, lat, lon),
-            headers=headers_request)
+            headers=headers)
     if r.status_code != 200:
         return render_template('templates/error.html', erreur=r.json()['message'])
     return render_template('index.html',
-            taxis=r.json()['data'], lat=lat, lon=lon, get_location=get_location)
+            taxis=r.json()['data'], lat=lat, lon=lon, get_location=get_location,
+            apikey=headers['X-API-KEY'])
 
 @app.route('/hail/')
 def hail():
@@ -64,16 +79,18 @@ def hail():
             }
         ]
     }
-    r = requests.post('{}/hails/'.format(base_url), headers=headers_request_post,
+    headers = make_headers(True)
+    r = requests.post('{}/hails/'.format(base_url), headers=headers,
             data=json.dumps(payload))
     if r.status_code < 200 or r.status_code >= 300:
         return render_template('error.html', erreur=r.json()['message'])
-    return redirect('/hail/{}'.format(r.json()['data'][0]['id']))
+    return redirect('/hail/{}?apikey={}'.format(r.json()['data'][0]['id'],
+        headers['X-API-KEY']))
 
 @app.route('/hail/<hail_id>')
 def hail_id(hail_id):
-    r = requests.get('{}/hails/{}'.format(base_url, hail_id),
-            headers=headers_request)
+    headers = make_headers()
+    r = requests.get('{}/hails/{}'.format(base_url, hail_id), headers=headers)
     if r.status_code != 200:
         render_template('error.html', erreur=r.json()['message'])
     status = r.json()['data'][0]['status']
@@ -87,7 +104,8 @@ def hail_id(hail_id):
         return render_template('hail_waiting.html', status=map_first_status[status])
 
     if status == 'accepted_by_taxi':
-        return render_template('accept_hail.html', hail_id=hail_id)
+        return render_template('accept_hail.html', hail_id=hail_id,
+                apikey=headers['X-API-KEY'])
     final_status = {
             'declined_by_taxi': u'Le taxi a refusé la course',
             'accepted_by_customer': u'Le taxi arrive',
@@ -110,12 +128,12 @@ def accept_hail(hail_id):
     payload = {'data': [
         {'status': 'accepted_by_customer'}
     ]}
-    r = requests.put('{}/hails/{}/'.format(base_url, hail_id),
-            headers=headers_request_post,
+    headers = make_headers(True)
+    r = requests.put('{}/hails/{}/'.format(base_url, hail_id), headers=headers,
             data=json.dumps(payload))
     if r.status_code < 200 or r.status_code >= 300:
         return render_template('error.html', erreur=r.json()['message'])
-    return redirect('/hail/{}'.format(hail_id))
+    return redirect('/hail/{}?apikey={}'.format(hail_id, headers['X-API-KEY']))
 
 
 if __name__ == "__main__":
